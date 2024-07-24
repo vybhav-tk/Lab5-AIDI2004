c5217f06abadc97ec91ae18cb0028e99b82475e8
vybhav-tk <vybhav.tk@gmail.com>
2024-07-24 18:21:08 -0400

    All files commit

---
+++ app.py
@@ -0,0 +1,41 @@
+# app.py
+
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the model and label encoder
+with open('svr_model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+with open('label_encoder.pkl', 'rb') as le_file:
+    label_encoder = pickle.load(le_file)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    species = request.form['species']
+    length1 = float(request.form['length1'])
+    length2 = float(request.form['length2'])
+    length3 = float(request.form['length3'])
+    height = float(request.form['height'])
+    width = float(request.form['width'])
+
+    # Encode the species
+    species_encoded = label_encoder.transform([species])[0]
+
+    # Prepare the features
+    features = np.array([[species_encoded, length1, length2, length3, height, width]])
+
+    # Predict the weight
+    prediction = model.predict(features)
+
+    return render_template('index.html', prediction_text='Predicted Weight: {:.2f} grams'.format(prediction[0]))
+
+if __name__ == "__main__":
+    app.run(debug=True)

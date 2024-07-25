745bfad9884746de6b37721736024a4456b4be9c
vybhav-tk <vybhav.tk@gmail.com>
2024-07-25 13:30:13 -0400

    Changes to app.py and requirements

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-# app.py
-
 from flask import Flask, request, render_template
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -38,4 +37,5 @@ def predict():
     return render_template('index.html', prediction_text='Predicted Weight: {:.2f} grams'.format(prediction[0]))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
